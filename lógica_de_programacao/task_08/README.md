ac7e210c6ab1d101468875497410ccf1a9d99a2f
Wanessa Alves <wanessaalves__@hotmail.com>
2021-06-13 19:26:05 -0300

    add readme and task_08

---
+++ lógica_de_programacao/task_08/README.md
@@ -0,0 +1,2 @@
+8. Criar uma função que receba um array como parâmetro e retorne ele com os valores
+invertinos.
---
+++ lógica_de_programacao/task_08/task08.js
@@ -0,0 +1,4 @@
+const invert = (arrayToInvert) => {
+    return arrayToInvert.reverse()
+}
+console.log(invert([13, 42, 33, 69]))
